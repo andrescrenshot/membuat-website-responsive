4bfe797626a27a6401b21d91ed1f49e7944cbea4
andrescrenshot <gaharugabut@gmail.com>
2025-09-22 14:32:14 +0700

    membuat website responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,74 @@
+import { useEffect, useState } from "react";
 
+const Navbar = () => {
+  const [show, setShow] = useState(false)
+  const [scroll, setScroll] = useState(false)
 
-export const Navbar = () => {
+    const handleclick = () => {
+      setShow(!show);
+      console.log(show)
+    }
+
+  let menuActive = show ? "left-0" : "-left-full"
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+
+      if (window.scrollY > 5) {
+        console.log("testing")
+        setScroll(true)
+        setShow(false)
+      } else{
+        setScroll(false)
+      }
+    });
+  });
+  
+  let scrollActive = scroll ? "py-6 bg-white shadow" : "py-4"
+
   return (
-    <div className="Navbar fixed w-full transition-all py-4">
+    <div className={`Navbar fixed w-full transition-all ${scrollActive}`}>
       <div className="container mx-auto px-4">
         <div className="navbar-box flex items-center justify-between">
           <div className="logo">
-            <h1 className="text-2xl font-bold">HOME</h1>
+            <h1 className="sm:text-2xl text-xl text-2xl font-bold">HOME</h1>
           </div>
-          <ul className="flex lg:gap-12 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full
-          md:translate-y-0 md:text-black md:p-0 md:m-0 md:transition-none gap-8 fixed left-0 top-1/2 -translate-y-1/2
-          flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-sky-400 font-bold text-white transition-all">
-            <li>
+          <ul className={`flex lg:gap-12 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full
+          md:translate-y-0 md:text-black md:p-0 md:m-0 md:transition-none gap-8 fixed ${menuActive} top-1/2
+           -translate-y-1/2
+          flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-sky-400 font-bold text-white transition-all`}>
+            <li className="flex items-center gap-3">
+              <li className="ri-home-smile-line text3xl md:hidden block"></li>
               <a href="#" className="font-medium opacity-75">
-                Beranda
-              </a>
+Beranda </a>
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+               <li className="ri-information-line text3xl md:hidden block"></li>
               <a href="#" className="font-medium opacity-75">
                 Tentang kami
               </a>
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+               <li className="ri-settings-3-line text3xl md:hidden block"></li>
               <a href="#" className="font-medium opacity-75">
                 Layanan
               </a>
             </li>
 
-            <li>
+            <li className="flex items-center gap-3">
+               <li className="ri-image-line text3xl md:hidden block"></li>
               <a href="#" className="font-medium opacity-75">
                 Proyek
               </a>
             </li>
           </ul>
-          <div className="social">
+          <div className="social flex items-center gap-2">
             <a
               href="#"
               className="bg-sky-300 px-5 py-2 rounded-full text-white font-bold hover:bg-sky-500 transition-all">
               Social Media
             </a>
-            <i className="ri-menu-line"></i>
+            <i className="ri-menu-line text-3xl md:hidden block" onClick={handleclick}></i>
           </div>
         </div>
       </div>
